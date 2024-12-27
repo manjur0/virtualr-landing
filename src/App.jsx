8245b8f23d7e6bc3ddc6ac730e12374375bc34e9
manjur0 <sfmanjur21@gmail.com>
2024-12-27 22:01:02 +0600

    Hero section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import HeroSection from "./Components/HeroSection";
 import Navbar from "./Components/Navbar";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+      </div>
     </div>
   );
 };
